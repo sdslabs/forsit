40e563705b840e65d60d7b5a35fff85ab65e138a
jaindeepali <djdeepalijain811@gmail.com>
2014-10-25 21:45:19 +0530

    Function to recommend correlated problems

--- problem.py
+++ problem.py
@@ -18,10 +18,16 @@ try:
 except ImportError as exc:
     print("Error: failed to import settings module ({})".format(exc))
 
+try:
+    import math
+except ImportError as exc:
+    print("Error: failed to import settings module ({})".format(exc))
+
 class problem(base):
 	def __init__(self, pid):
 		self.pid = str(pid)
 		self.fetch_info()
+		self.create_difficulty_matrix()
 
 	def fetch_info(self):
 		sql = "SELECT points, correct_count, attempt_count, (SELECT MAX(points) FROM problem WHERE MID(pid,1,3) = \'" + self.pid[0:3] + "\') AS max_points FROM problem WHERE pid = \'" + self.pid + "\'"
@@ -119,39 +125,62 @@ class problem(base):
 		sql+=" AND difficulty > 0"
 		return self.reco_algo(sql)
 
-	def find_correlation(self, user_prefs, p1, p2):
-		transform(user_prefs)
-		# Get the list of mutually rated items
-		si={}
-		for item in user_prefs[u1]:
-			if item in user_prefs[u2]: si[item]=1
+	def create_difficulty_matrix(self):
+		self.difficulty_matrix = {}
+		conn = db.connect('forsit')
+		cursor=conn.cursor()
+		sql = "SELECT * FROM activity"
+		result = db.read(sql, cursor)
+		for i in result:
+			user = str(i[0].encode('utf8'))
+			problem = str(i[1].encode('utf8'))
+			if not self.difficulty_matrix.has_key(problem):
+				self.difficulty_matrix[problem] = {}
+			self.difficulty_matrix[problem][user] = i[4]
 
+	def find_correlation(self, u1, u2):
+
+		# Get the list of mutually rated items
+		si = {}
+		for item in self.difficulty_matrix[u1]:
+			if item in self.difficulty_matrix[u2]:
+				si[item] = 1
 		# Find the number of elements
-		n=len(si)
+		n = len(si)
 
 		# if they are no ratings in common, return 0
-		if n==0: return 0
+		if n == 0:
+			return 0
 
 		# Add up all the preferences
-		sum1=sum([user_prefs[u1][it] for it in si])
-		sum2=sum([user_prefs[u2][it] for it in si])
+		sum1 = sum([self.difficulty_matrix[u1][it] for it in si])
+		sum2 = sum([self.difficulty_matrix[u2][it] for it in si])
 
 		# Sum up the squares
-		sum1Sq=sum([pow(user_prefs[u1][it],2) for it in si])
-		sum2Sq=sum([pow(user_prefs[u2][it],2) for it in si])
+		sum1Sq = sum([pow(self.difficulty_matrix[u1][it],2) for it in si])
+		sum2Sq = sum([pow(self.difficulty_matrix[u2][it],2) for it in si])
 
-		# Sum up the weights
-		pSum=sum([user_prefs[u1][it]*user_prefs[u2][it] for it in si])
+		# Sum up the products
+		pSum = sum([self.difficulty_matrix[u1][it] * self.difficulty_matrix[u2][it] for it in si])
 
 		# Calculate Pearson score
-		num=pSum-(sum1*sum2/n)
-		den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+		num = pSum - (sum1*sum2/n)
+		den = math.sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
 
-		if den==0: return 0
-		r=num/den
+		if den == 0:
+			return 0
+		r = num/den
 		return r
 
-# a = problem('erd1')
+	def find_correlated_problems(self):
+		self.correlated_problems = {}
+		for p in self.difficulty_matrix.keys():
+			if p != self.pid:
+				self.correlated_problems[p] = self.find_correlation(self.pid, p)
+		self.correlated_problems = sorted(self.correlated_problems.items(), key=operator.itemgetter(1), reverse = 1)
+
+a = problem('erd13')
 # a.fetch_info()
 # a.print_info()
-# a.find_similar_cfs(1)
+print a.find_similar_erdos(1)
+a.find_correlated_problems()
