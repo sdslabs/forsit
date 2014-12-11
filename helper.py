5fc4d0c692717934e58996cf8148e50937ee8193
Shagun <sshagunsodhani@gmail.com>
2014-12-11 23:48:41 +0530

    documented helper.py and db.py

--- helper.py
+++ helper.py
@@ -2,14 +2,27 @@ try:
     import random
 except ImportError as exc:
     print("Error: failed to import settings module ({})".format(exc))
- 
+
 def get_random_color(pastel_factor = 0.5):
-    return [round( (x+pastel_factor)/(1.0+pastel_factor), 6) for x in [random.uniform(0,1.0) for i in [1,2,3]]]
- 
+	'''
+	|  Return a random color represented as a list of form [a,b,c] where a,b,c correspond to RGB values of the random color
+	'''
+	return [round( (x+pastel_factor)/(1.0+pastel_factor), 6) for x in [random.uniform(0,1.0) for i in [1,2,3]]]
+
 def color_distance(c1,c2):
-    return sum([abs(x[0]-x[1]) for x in zip(c1,c2)])
+	'''
+	|  Return a distance-like measure given 2 colors c1 and c2
+	|  Colors are represented as a list of form [a,b,c] where a,b,c correspond to RGB values
+	'''
+	return sum([abs(x[0]-x[1]) for x in zip(c1,c2)])
 
 def generate_new_color(N,pastel_factor = 0.5):
+	'''
+    Input 
+	- N : Number of colors of be returned
+	- pastel_factor : pastel factor of the colors
+    Return a list of n colors where each color is represented as a list of form [a,b,c] where a,b,c correspond to RGB values
+	'''
 	colors_ret = []
 	color = get_random_color(pastel_factor = pastel_factor)
 	colors_ret.append(color)
