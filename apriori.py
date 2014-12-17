74b378906d12b5129abc02fd40adca291d99aac4
Shagun <sshagunsodhani@gmail.com>
2014-12-17 19:36:44 +0530

    Added method to add frequent itemsets

--- apriori.py
+++ apriori.py
@@ -39,15 +39,18 @@ class apriori():
     - *itemset* : set
 	- *frequency_itemset* : {}
     - *association_rules* : {}
-
+	- *size_one_candidate_set* : set
+	- *max_iterations*
+	count_transaction
 	'''
 	
-	def __init__(self, app_name = "forsit", min_support = 0.05, min_confidence = 0.95):
+	def __init__(self, app_name = "forsit", min_support = 0.05, min_confidence = 0.95, max_iterations = 10):
 		
 		self.min_support = min_support
 		self.min_confidence = min_confidence
 		self.conn = db.connect(app_name)
 		self.cursor = self.conn.cursor()
+		self.max_iterations = max_iterations
 
 	def initialise(self):
 		'''
@@ -64,23 +67,23 @@ class apriori():
 			self.transaction.append(transaction_temp)
 			for j in i:
 				self.itemset.add(j)	
+		self.count_transaction = len(self.transaction)
 
-	def prune(self):
+	def prune(self, itemset = self.itemset):
 		'''
 		|  Prune subset of itemset based on min_support
 		'''
 		result = set()
 		localset = {}
 	    
-		for item in self.itemset:
+		for item in itemset:
 			for t in self.transaction:
 				if item.issubset(t):
 					self.frequency_itemset[item]+=1
 					localset[item]+=1
 
-		size_transaction = len(self.transaction)			
 		for item in localset:
-			support = float(localset[item])/size_transaction
+			support = float(localset[item])/self.count_transaction
 			if support >= self.minSupport:
 				result.add(item)
 
@@ -93,5 +96,26 @@ class apriori():
 		'''
 		self.initialise()
 		self.frequency_itemset = {}
-        self.association_rules = {}
-		self.prune()        
+        self.association_rules = {} 
+        self.size_itemset = {}
+		# Dictionary mapping itemset size with itemsets
+
+		self.size_one_candidate_set = self.prune()
+		current_candidate_set = self.size_one_candidate_set
+		k = 2
+		while(currentLSet != set([]) or k!=self.max_iterations):
+			size_itemset[k-1] = currentLSet
+			current_candidate_set = helper.joinSet(current_candidate_set, k)
+			current_candidate_set = self.prune(current_candidate_set)
+			k+=1
+
+		def support(item):
+			"""local function which Returns the support of an item"""
+			return float(frequency_itemset[item])/self.count_transaction
+
+		self.Final_item = []
+
+		for size in size_itemset:
+			for item in size_itemset[size]:
+				self.Final_item.append((tuple(item), support(item)))
+
