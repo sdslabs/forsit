4dc23b398a36efa4ab0a9eb9d534b348c66363a1
Shagun <sshagunsodhani@gmail.com>
2015-01-28 13:45:32 +0530

    updated installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-apt-get install build-essential python-dev libmysqlclient-dev subversion libv8-dev libboost-all-dev
-svn checkout http://v8.googlecode.com/svn/trunk/ ~/v8
-cd ~/v8
-make builddeps
-make native
-cd -
-pip install -r requirements.txt
+apt-get install build-essential python-dev libmysqlclient-dev
+pip install beautifulsoup4 requests MySQL-python
