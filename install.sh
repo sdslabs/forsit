c6ac8b2fcadbc3cd815c85528931e4fcf650beb0
Shagun <sshagunsodhani@gmail.com>
2015-02-03 14:15:26 +0530

    Added instructions to install matplotlib

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get install build-essential python-dev libmysqlclient-dev
+apt-get install build-essential python-dev libmysqlclient-dev python-matplotlib
 pip install beautifulsoup4 requests MySQL-python
