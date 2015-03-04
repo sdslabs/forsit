9c1da3eda10904395bd6cf0de12c932377b91a17
Shagun <sshagunsodhani@gmail.com>
2015-03-04 14:25:58 +0530

    split scrapper script into functions and using dict instead of list for faster lookups

--- scrapper/erdos.py
+++ scrapper/erdos.py
@@ -1,14 +1,9 @@
 #! /usr/bin/python
 
-try:
-    import os
-except ImportError as exc:
-    print("Error: failed to import settings module ({})".format(exc))
-
-try:
-    import sys
-except ImportError as exc:
-    print("Error: failed to import settings module ({})".format(exc))
+import os
+import sys
+from time import time
+from time import sleep
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -17,22 +12,12 @@ if not path in sys.path:
 del path
 
 try:
-    import db
-except ImportError as exc:
-    print("Error: failed to import settings module ({})".format(exc))
-
-try:
     import requests
 except ImportError as exc:
     print("Error: failed to import settings module ({})".format(exc))
 
 try:
-    from time import time
-except ImportError as exc:
-    print("Error: failed to import settings module ({})".format(exc))
-
-try:
-    from time import sleep
+    import db
 except ImportError as exc:
     print("Error: failed to import settings module ({})".format(exc))
 
@@ -43,36 +28,43 @@ except ImportError as exc:
 
 print "script was run at ", time()
 
-# requests = cfscrape.create_requests()
-
-
-tags = []
+# tags = []
+tags = {}
 problem_list = []
-problem_db = []
+# problem_db = []
+problem_db = {}
+
 
 conn = db.connect()
 cursor=conn.cursor()
+
 remote_conn = db.connect('remote')
 remote_cursor = remote_conn.cursor()
 
-def fetch_all():
-
+def fetch_problems():
+    
     sql = "SELECT pid from problem WHERE MID(pid,1,3)=\"erd\""
     a = db.read(sql, cursor)
     for i in a:
         temp = str(i[0].encode('utf8'))
-        problem_db.append(temp)
+        # problem_db.append(temp)
+        problem_db[temp] = 0
 
     sql = "SELECT tag from tag"
     a = db.read(sql, cursor)
     for i in a:
         tag = str(i[0].encode('utf8'))
         tags.append(tag)
-    tag_sql = ""
-    ptag_sql = ""
+        tags[tag] = 0
+    
+    tag_sql = "INSERT INTO tag (tag, description, time, count) VALUES "
+    ptag_sql = "INSERT INTO ptag (pid, tag) VALUES "
     problem_sql = "INSERT INTO problem (pid, name, attempt_count, correct_count, time) VALUES "
+
     tag_url = "http://erdos.sdslabs.co/tags.json"
+    
     tag_r = requests.get(tag_url)
+    
     if(tag_r.status_code != 200 ):
         print tag_r.status_code, " returned from ", tag_url
     else:
@@ -81,7 +73,9 @@ def fetch_all():
             # print tag
             tag = str(i['name'])
             tag_count = str(i['count'])
+            tag_flag = 1
             if(tag not in tags):
+                tag_flag = 0
                 tag_sql+="('" + tag + "','','"  + str(int(time())) + "','" + tag_count + "'), "
             ptag_url = "http://erdos.sdslabs.co/tags/"+tag+".json"
             ptag_r = requests.get(ptag_url)
@@ -93,46 +87,48 @@ def fetch_all():
                     code = str(i['id'])
                     code = code.replace('"','\\"')
                     code = code.replace("'","\\'")
-                    problem_url = "http://erdos.sdslabs.co/problems/"+code+".json"
-                    prob = requests.get(problem_url)
-                    prob = prob.json()['submissions']
-                    correct = prob['correct']
-                    total = prob['total']
-                    code ="erd"+code
-                    name = str(i['name'].encode('utf8'))
-                    name = name.replace('"','\\"')
-                    name = name.replace("'","\\'")
-                    if(code not in problem_list):
+                    if (code in problem_db and problem_db[code] == 0) or code not in problem_db :
+                        #(problem already in db but not updated) or (is not in the db)
+                        problem_url = "http://erdos.sdslabs.co/problems/"+code+".json"
+                        prob = requests.get(problem_url)
+                        prob = prob.json()['submissions']
+                        correct = prob['correct']
+                        total = prob['total']
+                        code ="erd"+code
+                        name = str(i['name'].encode('utf8'))
+                        name = name.replace('"','\\"')
+                        name = name.replace("'","\\'")
+                        problem_db[code] = 1
                         problem_list.append(code)
                         problem_sql+="('" + code + "','" + name + "','" + str(total) + "','" + str(correct) + "','" + str(int(time())) + "'), "
-                    if code not in problem_db:
-                        ptag_sql+="('"+code+"', '"+tag+"'), "
+                    if not tag_flag:
+                        ptag_sql+="('"+code+"', '"+tag+"'), "             
 
         if(tag_sql!=""):
             tag_sql = tag_sql[:-2]
-            tag_sql = "INSERT INTO tag (tag, description, time, count) VALUES " + tag_sql
+            # tag_sql =  + tag_sql
             print tag_sql
             db.write(tag_sql, cursor, conn)
-
-        problem_sql = problem_sql[:-2]
-        problem_sql+=" ON DUPLICATE KEY UPDATE attempt_count=VALUES(attempt_count),correct_count=VALUES(correct_count),time=VALUES(time);"
-        print problem_sql
-        db.write(problem_sql, cursor, conn)
+        if(problem_sql!=""):
+            problem_sql = problem_sql[:-2]
+            problem_sql+=" ON DUPLICATE KEY UPDATE attempt_count=VALUES(attempt_count),correct_count=VALUES(correct_count),time=VALUES(time);"
+            print problem_sql
+            db.write(problem_sql, cursor, conn)
         
         if(ptag_sql!=""):
             ptag_sql = ptag_sql[:-2]
-            ptag_sql = "INSERT INTO ptag (pid, tag) VALUES " + ptag_sql
+            # ptag_sql =  + ptag_sql
             print ptag_sql
             db.write(ptag_sql, cursor, conn)
 
+def fetch_users():
     sql_user = "SELECT MID(erd_handle,4) FROM user"
     result = db.read(sql_user, cursor)
     user_list = {}
-    #using dict for fast lookup
+    #using dict for faster lookup
     for i in result:
         user_list[i[0]]=0
     new_user = []
-
     done = 0
     total = 1
     page = 1
@@ -159,30 +155,6 @@ def fetch_all():
         print sql
         db.write(sql, cursor, conn)
         db.write(sql, remote_cursor, remote_conn)
-    sleep(3)
-
-def fetch_user_list_erd():
-    '''
-    |  Fetch List of all the users from Erdos
-    '''
-    erd_users = []
-    done = 0
-    total = 1
-    page = 1
-    while ( done < total ):
-        url = "http://erdos.sdslabs.co/users.json?page=" + str(page)
-        r = requests.get(url)
-        if(r.status_code != 200 ):
-            print r.status_code, " returned from ", r.url
-        else:
-            total = r.json()['TOTAL']
-            done += 500
-            result = r.json()['list']
-            for i in result:
-                erd_users.append(i['username'])
-        page += 1
-    # print len(erd_users)
-    return erd_users
 
 def fetch_user_activity_erd(uid="", handle=""):
     '''
@@ -190,14 +162,19 @@ def fetch_user_activity_erd(uid="", handle=""):
     '''
     url = "http://erdos.sdslabs.co/activity/users/" + handle[3:] + ".json"
     print url
-    sql = "SELECT created_at FROM activity WHERE handle = \'" + handle + "\' ORDER BY created_at DESC LIMIT 1;"
+    
+    start_time 
+
+    sql = "SELECT MAX(created_at) FROM activity WHERE handle = \'" + handle + "\'"
+    print sql
     res = db.read(sql, cursor)
     if res == ():
         last_activity = 0
     else:
         last_activity = res[0][0]
     last_activity = int(last_activity)
-    r = requests.get(url)
+    params['start_time'] = last_activity 
+    r = requests.get(url, params)
     if(r.status_code != 200 ):
         print r.status_code, " returned from ", r.url
     else:
@@ -228,7 +205,8 @@ def fetch_user_activity_erd(uid="", handle=""):
                     sql = "UPDATE activity SET attempt_count = attempt_count + 1, status = " + str(act['status']) + ", difficulty = " + str(difficulty) + ", created_at = " + str(act['created_at']) + " WHERE pid = \'erd" + act['problem_id'] + "\' AND handle = \'" + handle + "\'"
                     db.write(sql, cursor, conn)
             print sql
-def fetch_user_activity_all():
+
+def fetch_activity():
     # erd_users = fetch_user_list_erd()
     sql = "SELECT uid, erd_handle FROM user"
     erd_users = db.read(sql, cursor)
@@ -241,13 +219,10 @@ def fetch_user_activity_all():
           (SELECT SUM((correct_count-3)/attempt_count) FROM problem WHERE pid IN \
           (SELECT DISTINCT(pid) FROM activity WHERE uid = user.uid AND MID(pid,1,3)=\'erd\' AND status = 1)\
           AND correct_count>3)"
-
     # print sql
     db.write(sql, cursor, conn)
 
-# fetch_user_list_erd()
-fetch_all()
-fetch_user_activity_all()
-# fetch_user_activity_erd("TheOrganicGypsy")
-# fetch_user_list_erd()
+fetch_problems()
+fetch_users()
+fetch_activity()
 cursor.close()
