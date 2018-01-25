c8b30192ed1921e7e4936346d9fd1a8cb0fb4df8
alphadose <anish.mukherjee1996@gmail.com>
2018-01-25 19:25:39 +0530

    Updated Schema

--- schema/forsit.sql
+++ schema/forsit.sql
@@ -1,64 +1,82 @@
--- phpMyAdmin SQL Dump
--- version 3.5.8.1deb1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: Mar 17, 2015 at 02:07 AM
--- Server version: 5.5.34-0ubuntu0.13.04.1
--- PHP Version: 5.4.9-4ubuntu2.4
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: forsit
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `forsit`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `activity`
 --
 
-CREATE TABLE IF NOT EXISTS `activity` (
+DROP TABLE IF EXISTS `activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activity` (
   `handle` varchar(100) NOT NULL,
   `pid` varchar(100) NOT NULL,
   `attempt_count` int(11) NOT NULL DEFAULT '0',
   `status` int(11) NOT NULL DEFAULT '0',
   `difficulty` double NOT NULL DEFAULT '0',
-  `uid` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '0',
   `created_at` int(12) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `activity`
+--
+
+LOCK TABLES `activity` WRITE;
+/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `activity_concept`
 --
 
-CREATE TABLE IF NOT EXISTS `activity_concept` (
+DROP TABLE IF EXISTS `activity_concept`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activity_concept` (
   `handle` varchar(100) NOT NULL,
   `pid` varchar(100) NOT NULL,
   `attempt_count` int(11) NOT NULL DEFAULT '0',
   `status` int(11) NOT NULL DEFAULT '0',
   `difficulty` double NOT NULL DEFAULT '0',
-  `uid` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '0',
   `created_at` bigint(20) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `activity_concept`
+--
+
+LOCK TABLES `activity_concept` WRITE;
+/*!40000 ALTER TABLE `activity_concept` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activity_concept` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `problem`
 --
 
-CREATE TABLE IF NOT EXISTS `problem` (
+DROP TABLE IF EXISTS `problem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problem` (
   `pid` varchar(50) NOT NULL,
   `name` varchar(200) NOT NULL,
   `contestId` varchar(50) NOT NULL,
@@ -73,15 +91,26 @@ CREATE TABLE IF NOT EXISTS `problem` (
   KEY `contestId` (`contestId`),
   KEY `points` (`points`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `problem`
+--
+
+LOCK TABLES `problem` WRITE;
+/*!40000 ALTER TABLE `problem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `problem` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `problem_reco`
 --
 
-CREATE TABLE IF NOT EXISTS `problem_reco` (
-  `uid` varchar(100) NOT NULL,
+DROP TABLE IF EXISTS `problem_reco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problem_reco` (
+  `user_id` varchar(100) NOT NULL,
   `base_pid` varchar(100) NOT NULL,
   `status` int(11) NOT NULL,
   `reco_pid` varchar(100) NOT NULL,
@@ -91,39 +120,73 @@ CREATE TABLE IF NOT EXISTS `problem_reco` (
   `is_deleted` int(11) NOT NULL,
   `state` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `problem_reco`
+--
+
+LOCK TABLES `problem_reco` WRITE;
+/*!40000 ALTER TABLE `problem_reco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `problem_reco` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `ptag`
 --
 
-CREATE TABLE IF NOT EXISTS `ptag` (
+DROP TABLE IF EXISTS `ptag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ptag` (
   `pid` varchar(50) NOT NULL,
   `tag` varchar(100) NOT NULL,
   PRIMARY KEY (`pid`,`tag`),
-  KEY `tag` (`tag`)
+  KEY `tag` (`tag`),
+  CONSTRAINT `ptag_ibfk_2` FOREIGN KEY (`tag`) REFERENCES `tag` (`tag`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `ptag`
+--
+
+LOCK TABLES `ptag` WRITE;
+/*!40000 ALTER TABLE `ptag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ptag` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `similar_users`
 --
 
-CREATE TABLE IF NOT EXISTS `similar_users` (
-  `uid` int(11) NOT NULL,
+DROP TABLE IF EXISTS `similar_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `similar_users` (
+  `user_id` int(11) NOT NULL,
   `similar_user` varchar(300) NOT NULL,
   `similarity` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `similar_users`
+--
+
+LOCK TABLES `similar_users` WRITE;
+/*!40000 ALTER TABLE `similar_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `similar_users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tag`
 --
 
-CREATE TABLE IF NOT EXISTS `tag` (
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
   `tag` varchar(100) NOT NULL,
   `description` varchar(250) NOT NULL,
   `time` int(11) NOT NULL,
@@ -131,30 +194,52 @@ CREATE TABLE IF NOT EXISTS `tag` (
   PRIMARY KEY (`tag`),
   UNIQUE KEY `tag` (`tag`,`description`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `tag`
+--
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `erd_handle` varchar(50) NOT NULL,
   `cfs_handle` varchar(50) NOT NULL,
   `erd_score` double NOT NULL,
   `cfs_score` double NOT NULL,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9059 ;
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9059 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_reco`
 --
 
-CREATE TABLE IF NOT EXISTS `user_reco` (
-  `uid` varchar(100) NOT NULL,
+DROP TABLE IF EXISTS `user_reco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_reco` (
+  `user_id` varchar(100) NOT NULL,
   `pid` varchar(100) NOT NULL,
   `score` double NOT NULL,
   `time_created` int(11) NOT NULL,
@@ -162,29 +247,47 @@ CREATE TABLE IF NOT EXISTS `user_reco` (
   `is_deleted` int(11) NOT NULL,
   `state` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `user_reco`
+--
+
+LOCK TABLES `user_reco` WRITE;
+/*!40000 ALTER TABLE `user_reco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_reco` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_tag_score`
 --
 
-CREATE TABLE IF NOT EXISTS `user_tag_score` (
+DROP TABLE IF EXISTS `user_tag_score`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_tag_score` (
   `handle` varchar(100) NOT NULL,
   `tag` varchar(100) NOT NULL,
   `score` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Constraints for dumped tables
+-- Dumping data for table `user_tag_score`
 --
 
---
--- Constraints for table `ptag`
---
-ALTER TABLE `ptag`
-  ADD CONSTRAINT `ptag_ibfk_2` FOREIGN KEY (`tag`) REFERENCES `tag` (`tag`) ON DELETE CASCADE ON UPDATE CASCADE;
+LOCK TABLES `user_tag_score` WRITE;
+/*!40000 ALTER TABLE `user_tag_score` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_tag_score` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-25 19:25:19
