017088fd1222ea2bdb5235559eba1c799159fdb6
alphadose <anish.mukherjee1996@gmail.com>
2018-01-25 20:25:22 +0530

    Added migrations

---
+++ schema/migrations/shift_to_user_id.sql
@@ -0,0 +1,7 @@
+ALTER TABLE activity RENAME COLUMN uid TO user_id;
+ALTER TABLE activity_concept RENAME COLUMN uid TO user_id;
+ALTER TABLE problem_reco RENAME COLUMN uid TO user_id;
+ALTER TABLE similar_users RENAME COLUMN uid TO user_id;
+ALTER TABLE user RENAME COLUMN uid TO user_id;
+ALTER TABLE user_reco RENAME COLUMN uid TO user_id;
+
